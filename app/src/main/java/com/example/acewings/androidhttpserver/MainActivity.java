303f1e1f67c83d80c0b26cf75f699e55c56ce4de
Acewings <khaireddinessarraj@gmail.com>
2017-12-14 23:47:14 +0100

    V_1

---
+++ app/src/main/java/com/example/acewings/androidhttpserver/MainActivity.java
@@ -0,0 +1,173 @@
+package com.example.acewings.androidhttpserver;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText welcomeMsg;
+    TextView infoIp;
+    TextView infoMsg;
+    String msgLog = "";
+    
+    ServerSocket httpServerSocket;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        welcomeMsg = (EditText) findViewById(R.id.welcomemsg);
+        infoIp = (TextView) findViewById(R.id.infoip);
+        infoMsg = (TextView) findViewById(R.id.msg);
+
+
+
+        infoIp.setText(getIpAddress() + ":"
+                + HttpServerThread.HttpServerPORT + "\n");
+
+        HttpServerThread httpServerThread = new HttpServerThread();
+        httpServerThread.start();
+    }
+
+
+
+    private String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "SiteLocalAddress: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+    private class HttpServerThread extends Thread {
+
+        static final int HttpServerPORT = 8888;
+
+        @Override
+        public void run() {
+            Socket socket = null;
+
+            try {
+                httpServerSocket = new ServerSocket(HttpServerPORT);
+
+                while(true){
+                    socket = httpServerSocket.accept();
+
+                    HttpResponseThread httpResponseThread =
+                            new HttpResponseThread(
+                                    socket,
+                                    welcomeMsg.getText().toString());
+                    httpResponseThread.start();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+
+    private class HttpResponseThread extends Thread {
+
+        Socket socket;
+        String h1;
+
+        HttpResponseThread(Socket socket, String msg){
+            this.socket = socket;
+            h1 = msg;
+        }
+
+        @Override
+        public void run() {
+            BufferedReader is;
+            PrintWriter os;
+            String request;
+
+
+            try {
+                is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                request = is.readLine();
+
+                os = new PrintWriter(socket.getOutputStream(), true);
+
+                String response =
+                        "<html><head></head>" +
+                                "<body>" +
+                                "<h1>"+h1+"</h1>" +
+                                "</body></html>";
+
+                os.print("HTTP/1.0 200" + "\r\n");
+                os.print("Content type: text/html" + "\r\n");
+                os.print("Content length: " + response.length() + "\r\n");
+                os.print("\r\n");
+                os.print(response + "\r\n");
+
+
+
+
+                msgLog += "Request of " + request
+                        + " from " + socket.getInetAddress().toString() + "\n";
+                socket.close();
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        infoMsg.setText(msgLog);
+                    }
+                });
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return;
+        }
+    }
+
+}
